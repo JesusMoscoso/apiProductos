3cdc3d5370ac33bda2f665a5d8afe22b71fc78c2
JesusMoscoso <MoscosoJesus481@gamil.com>
2025-03-17 08:23:59 -0600

    correccion  en el detalle de la venta

--- ProductosApi - copia/ProductosApi/obj/Debug/net8.0/ProductosApi.AssemblyInfo.cs
+++ ProductosApi - copia/ProductosApi/obj/Debug/net8.0/ProductosApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c08c639af23e58d4048dff7f87c045a726204e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductosApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
